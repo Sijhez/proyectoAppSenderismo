4aad00ee1a0fcc271cf5ae4ef558afb0348d2481
GeorgeLR87 <jorgelrgarduno@gmail.com>
2021-11-29 11:26:33 -0600

    Inicializacion Proyecto App Senderismo

---
+++ index.js
@@ -0,0 +1,28 @@
+// 1 Importaciones
+const express         = require('express')
+const app             = express()
+const hbs             = require('hbs')
+const connectDB       = require('./config/db')
+
+require('dotenv').config()
+
+
+//2 Middlewares
+
+app.use(express.static("public"))
+app.set("views", __dirname + "/views")
+app.set("view engine", "hbs")
+
+hbs.registerPartials(__dirname + "/views/partials")
+
+
+connectDB()
+// 3 Rutas
+
+
+
+// 4 Servidor
+
+app.listen(process.env.PORT, () => {
+    console.log(`Servidor conectado en el puerto: http://localhost:${process.env.PORT}`)
+})
